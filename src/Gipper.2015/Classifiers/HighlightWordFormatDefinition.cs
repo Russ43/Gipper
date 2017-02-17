57901d97aeb01075caa27da526a00a25cb38faf0
Russell Froehlich <russ@company43.com>
2017-02-16 17:13:07 -0800

    implemented the Walkthrough: Highlighting Text example (https://msdn.microsoft.com/en-us/library/dd885121.aspx)

---
+++ src/Gipper.2015/Classifiers/HighlightWordFormatDefinition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System.Windows.Media;
+
+namespace Gipper._2015.Classifiers
+{
+	[Export(typeof(EditorFormatDefinition))]
+	[Name("MarkerFormatDefinition/HighlightWordFormatDefinition")]
+	[UserVisible(true)]
+	internal class HighlightWordFormatDefinition : MarkerFormatDefinition
+	{
+		public HighlightWordFormatDefinition()			
+		{
+			this.BackgroundColor = Colors.LightBlue;
+			this.ForegroundColor = Colors.DarkBlue;
+			this.DisplayName = "Highlight Word";
+			this.ZOrder = 5;
+		}
+	}
+}
