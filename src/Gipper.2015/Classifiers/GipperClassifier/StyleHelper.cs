02fbee94fb7e66e7e9959d706b42c89775cefa31
Russell <russ@company43.com>
2017-03-05 18:30:06 -0800

    changed fonts

--- src/Gipper.2015/Classifiers/GipperClassifier/StyleHelper.cs
+++ src/Gipper.2015/Classifiers/GipperClassifier/StyleHelper.cs
@@ -11,9 +11,9 @@ namespace Gipper._2015.Classifiers.GipperClassifier
 	internal class StyleHelper
 	{
 		public static readonly Typeface DefinitionFontFace = new Typeface("Segoe UI Black");
-		public static readonly Typeface CommentFontFace = new Typeface("Segoe Print");
+		public static readonly Typeface CommentFontFace = new Typeface("Short Stack");
 		public const double CommentFontRenderingSize = 13;
-		public static readonly Typeface LiteralFontFace = new Typeface(new FontFamily("Courier New"), FontStyles.Normal, FontWeights.Heavy, FontStretches.Normal);
+		public static readonly Typeface LiteralFontFace = new Typeface("Courier Prime");
 		public const double NamespaceFontRenderingSize = 30;
 		public const double TypeFontRenderingSize = 24;
 		public const double MemberFontRenderingSize = 18;
